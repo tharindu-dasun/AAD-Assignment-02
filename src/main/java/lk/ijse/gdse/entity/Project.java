57af1cbc3f03802c7b370f29c1fe61d37be59349
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-09-24 23:15:57 +0530

    entity added

--- src/main/java/lk/ijse/gdse/entity/Project.java
+++ src/main/java/lk/ijse/gdse/entity/Project.java
@@ -1,4 +1,19 @@
 package lk.ijse.gdse.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Project {
+    @Id
+    private String projectId;
+    private String projectName;
+    private String projectDescription;
+    private String projectDeadline;
+    @ManyToOne
+    private TechLead techLeadId;
 }
--- src/main/java/lk/ijse/gdse/entity/TechLead.java
+++ src/main/java/lk/ijse/gdse/entity/TechLead.java
@@ -1,4 +1,23 @@
 package lk.ijse.gdse.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+
 public class TechLead {
+    @Id
+    private String techLeadId;
+    private String techLeadName;
+    private String techLeadEmail;
+    private String techLeadAddress;
+    @OneToMany(mappedBy = "techLeadId" ,targetEntity = Project.class,cascade = CascadeType.ALL)
+    private List<Project> projectsList = new ArrayList<>();
 }
