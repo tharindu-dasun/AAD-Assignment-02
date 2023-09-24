5e1f39b238cfae74e22c4b078d57a61587b1db80
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-09-24 19:58:40 +0530

    dto update

--- src/main/java/lk/ijse/gdse/dto/ProjectDTO.java
+++ src/main/java/lk/ijse/gdse/dto/ProjectDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse.dto;
 
-public class ProjectDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProjectDTO implements Serializable,SuperDTO {
+    private String projectId;
+    private String projectName;
+    private String projectDescription;
+    private String projectdeadline;
+    private String teachLeadId;
 }
--- src/main/java/lk/ijse/gdse/dto/TechleadDTO.java
+++ src/main/java/lk/ijse/gdse/dto/TechleadDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.gdse.dto;
 
-public class TechleadDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TechleadDTO implements Serializable, SuperDTO {
+    private String techLeadId;
+    private String techLeadName;
+    private String techLeadEmail;
+    private String techLeadAddress;
 }
